8b1f391b2f171716dc3047503b1f5ed710904870
andrewmiladd <andrewmilad11@gmail.com>
2022-11-24 21:41:52 +0200

    Removed private products array

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -4,7 +4,6 @@ import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 @Injectable()
 export class ProductsService {
-    products: Product[] = [];
     constructor(@InjectModel("Product") private readonly productModel: Model<Product>) {}
     async addProduct(title: string, description: string, price: number) {
         const newProduct = new this.productModel({ title, description, price });
