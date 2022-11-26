013da7c044c444b9742ec54f459b438935daf7cb
Bichoy Messiha <bichoy.messiha@indigen.com>
2022-11-26 19:53:03 +0200

    refactor: remove redundant code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
 import { AppController } from "./controllers/app.controller";
 import { ProductsController } from "./controllers/products.controller";
 import { AppService } from "./services/app.service";
 import { ProductsService } from "./services/products.service";
-import { MongooseModule } from "@nestjs/mongoose";
-import { ProductSchema } from "./models/products.model";
-import { ConfigModule } from "@nestjs/config";
 
 const databaseConnection = `mongodb+srv://andrewmiladd:Mokey1998xx@cluster0.otdkquu.mongodb.net/first-test?retryWrites=true&w=majority`;
 @Module({
@@ -15,7 +14,6 @@ const databaseConnection = `mongodb+srv://andrewmiladd:Mokey1998xx@cluster0.otdk
             envFilePath: "../.env",
         }),
         MongooseModule.forRoot(databaseConnection),
-        MongooseModule.forFeature([{ name: "Product", schema: ProductSchema }]),
     ],
     controllers: [AppController, ProductsController],
     providers: [AppService, ProductsService],
